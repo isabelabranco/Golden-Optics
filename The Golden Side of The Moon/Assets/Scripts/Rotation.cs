1af8887d0fecb905d01c3d6a96f3d043a1912b55
Isabela Branco <bela.branco@gmail.com>
2016-09-03 03:37:22 -0300

    movimentação da lanterna em um arco de 180 graus

---
+++ The Golden Side of The Moon/Assets/Scripts/Rotation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0)) {
+
+            //Check if click was inside the torch's BoxCollider2D so that it can perform the next steps
+
+            var position = Camera.main.WorldToScreenPoint(transform.position);
+            var direction = Input.mousePosition - position;
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            if (angle > 90)
+            {
+                angle = 90;
+            }
+            if (angle < -90)
+            {
+                angle = -90;
+            }
+            var rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, Time.deltaTime * 180);
+        }
+	}
+}
